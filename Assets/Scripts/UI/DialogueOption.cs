7f7368a4d0ec7bd8f472e05b84e7ff7bfffe9716
Alexander <72340168+KoutsAlex@users.noreply.github.com>
2024-01-27 18:12:21 +0200

    Dialogue Prefab updates

--- Assets/Scripts/UI/DialogueOption.cs
+++ Assets/Scripts/UI/DialogueOption.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
 
 public class DialogueOption : MonoBehaviour
 {
     [SerializeField] private JokeSO jokeSo = default;
     // TextMeshPro text ref
+    [SerializeField] private TextMeshProUGUI dialogueOptionText = default;
 
     void Start()
     {
         // Use ref.text = jokeSo.jokeDescription;
+        dialogueOptionText.text = jokeSo.JokeDiscription;
     }
 }
