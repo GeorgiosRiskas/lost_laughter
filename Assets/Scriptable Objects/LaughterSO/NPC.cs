bb42a7cc7e580199d62c0f3308d2bd99da783ed1
GeorgiosRiskas <31712800+GeorgiosRiskas@users.noreply.github.com>
2024-01-27 13:29:28 +0200

    Added options to npcSo

--- Assets/Scriptable Objects/LaughterSO/NPC.cs
+++ Assets/Scriptable Objects/LaughterSO/NPC.cs
@@ -3,5 +3,9 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "npcX", menuName = "Lost Laughter/npc")]
 public class NpcSO : ScriptableObject
 {
+    public string dialogue_greeting;
+    public string dialogue_failure;
+    public string correctJokeId;
     public string laughterId;
+    public AudioClip laughterSfx;
 }
