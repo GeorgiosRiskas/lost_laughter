b30d6a947570cd3fedc4ba546119d17b4145be68
GeorgiosRiskas <31712800+GeorgiosRiskas@users.noreply.github.com>
2024-01-27 13:32:51 +0200

    So cleanup

--- Assets/Scriptable Objects/DialogueSO/DialogueObject.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "dialogueX", menuName = "DialogueObjectSO")]
-public class DialogueObject : ScriptableObject
-{
-    public List<string> dialogue = new List<string>();
-}
