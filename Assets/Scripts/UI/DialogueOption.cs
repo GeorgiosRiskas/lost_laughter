4f3048caf1e4b3b3bb6c26fbf8c776a03d950a8e
GeorgiosRiskas <31712800+GeorgiosRiskas@users.noreply.github.com>
2024-01-27 17:42:22 +0200

    Dialogue options

---
+++ Assets/Scripts/UI/DialogueOption.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueOption : MonoBehaviour
+{
+    [SerializeField] private JokeSO jokeSo = default;
+    // TextMeshPro text ref
+
+    void Start()
+    {
+        // Use ref.text = jokeSo.jokeDescription;
+    }
+}
